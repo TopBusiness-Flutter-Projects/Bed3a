e46239c34366fe6b9edda5f208733a8fea43b09b
ahmedelsapagh <ahmedelsapagh10@gmail.com>
2023-12-07 15:11:29 +0200

    AAAAAAAAAAA

--- lib/utill/images.dart
+++ lib/utill/images.dart
@@ -1,14 +1,15 @@
-
 class Images {
   static const String splash_logo = 'assets/images/logo_splash.png';
   static const String more_filled_image = 'assets/images/more_filled.png';
-  static const String cart_arrow_down_image = 'assets/images/cart_arrow_down.png';
+  static const String cart_arrow_down_image =
+      'assets/images/cart_arrow_down.png';
   static const String cart_image = 'assets/images/cart.png';
   static const String home_image = 'assets/images/home.png';
   static const String office_image = 'assets/images/office.png';
   static const String logo_image = 'assets/images/logo.png';
   static const String logo_with_name_image = 'assets/images/logo_with_name.png';
-  static const String logo_with_name_image_white = 'assets/images/logo_with_name_white.png';
+  static const String logo_with_name_image_white =
+      'assets/images/logo_with_name_white.png';
   static const String message_image = 'assets/images/inbox.png';
   static const String more_image = 'assets/images/more.png';
   static const String shopping_image = 'assets/images/order.png';
@@ -19,13 +20,15 @@ class Images {
   static const String filter_image = 'assets/images/filter.png';
   static const String fast_delivery = 'assets/images/fast_delivery.png';
   static const String delivery = 'assets/images/delivery.png';
-  static const String toolbar_background = 'assets/images/toolbar_background.jpg';
+  static const String toolbar_background =
+      'assets/images/toolbar_background.jpg';
   static const String EDIT_TWO = 'assets/images/edit_2.png';
   static const String CHAT = 'assets/images/chat.png';
   static const String chats = 'assets/images/chats.png';
   static const String help_center = 'assets/images/help_center.png';
   static const String language = 'assets/images/language.png';
-  static const String notification_filled = 'assets/images/notification_filled.png';
+  static const String notification_filled =
+      'assets/images/notification_filled.png';
   static const String notification = 'assets/images/notification.png';
   static const String offers = 'assets/images/offers.png';
   static const String preference = 'assets/images/preference.png';
@@ -54,7 +57,8 @@ class Images {
   static const String upload_image = 'assets/images/upload_image.png';
   static const String star = 'assets/images/star.png';
   static const String flash_deal = 'assets/images/flash_deal.png';
-  static const String seven_day_easy_return = 'assets/images/seven_day_return.png';
+  static const String seven_day_easy_return =
+      'assets/images/seven_day_return.png';
   static const String safe_payment = 'assets/images/safe_payment.png';
   static const String hundred_par_authentic = 'assets/images/authentic.png';
   static const String delete = 'assets/images/delete.png';
@@ -63,13 +67,14 @@ class Images {
   static const String placeholder_1x1 = 'assets/images/placeholder_1x1.png';
   static const String placeholder_3x1 = 'assets/images/placeholder_3x1.png';
   static const String wallet = 'assets/images/wallet.png';
-  static const String loyalty_point  = 'assets/images/trophy.png';
-  static const String loyalty_trophy  = 'assets/images/loyalty.png';
-  static const String file_download  = 'assets/images/cloud-download.png';
-  static const String callIcon  = 'assets/images/call_icon.png';
-  static const String smsIcon  = 'assets/images/sms_icon.png';
+  static const String loyalty_point = 'assets/images/trophy.png';
+  static const String loyalty_trophy = 'assets/images/loyalty.png';
+  static const String file_download = 'assets/images/cloud-download.png';
+  static const String callIcon = 'assets/images/call_icon.png';
+  static const String smsIcon = 'assets/images/sms_icon.png';
   static const String search_icon = 'assets/images/search_icon.png';
-  static const String splashScreenLogo = 'assets/images/logo_with_name_white.png';
+  static const String splashScreenLogo =
+      'assets/images/logo_with_name_white.png';
   static const String c_policy = 'assets/images/c_policy.png';
   static const String return_policy = 'assets/images/return_policy.png';
   static const String refund_policy = 'assets/images/refund_policy.png';
